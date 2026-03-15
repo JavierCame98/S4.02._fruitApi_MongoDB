dae4b5e7a79f609c13b1b4c21f2c212b6dbbf501
Javier Castillo <javiercame98@gmail.com>
2026-03-15 17:51:27 +0100

    feat: creating response dtos

---
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderItemResponseDto.java
@@ -0,0 +1,6 @@
+package cat.itacademy.s04.t02.n03.fruit.model;
+
+public record OrderItemResponseDto (
+        String fruitName,
+        int quantityInKilos
+){ }
---
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderResponseDto.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t02.n03.fruit.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OrderResponseDto(
+        String id,
+        String clientName,
+        LocalDate deliveryDate,
+        List<OrderItem>items
+) {
+}
